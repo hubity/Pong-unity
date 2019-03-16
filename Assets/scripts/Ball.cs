d6dac9759674250abed0d36d1e25f9d8bb2a3c7a
unknown <hubitygames@gmail.com>
2019-03-16 03:01:06 -0300

    add controller paddle, ball & sound effects

--- Assets/scripts/Ball.cs
+++ Assets/scripts/Ball.cs
@@ -4,13 +4,72 @@ using UnityEngine;
 
 public class Ball : MonoBehaviour {
 
+    public float speed = 30;
+
+    private Rigidbody2D rigidBody;
+
+    private AudioSource audioSouce;
+
 	// Use this for initialization
 	void Start () {
-		
+
+        rigidBody = GetComponent<Rigidbody2D>();
+        rigidBody.velocity = Vector2.right * speed;
 	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+
+     void OnCollisionEnter2D(Collision2D col)
+    {
+        //leftPaddle or RihtPaddle
+        if((col.gameObject.name == "LeftPaddle") || 
+            col.gameObject.name == "RightPaddle")
+        {
+            HandlePaddleHit(col);
+        }
+
+        //WallBottom oy WallTop
+        if ((col.gameObject.name == "WallBottom") ||
+            col.gameObject.name == "WallTop")
+        {
+
+            SoundManager.Instace.PlayOneShot(SoundManager.Instace.wallBloop);
+        }
+        //LeftGoal or RightGoal
+        if ((col.gameObject.name == "leftGoal") ||
+            col.gameObject.name == "RightGoal")
+        {
+            SoundManager.Instace.PlayOneShot(SoundManager.Instace.goalBloop);
+
+            // TODO upadate Score UI
+
+            transform.position = new Vector2(0,0);
+        }
+    }
+
+    void HandlePaddleHit(Collision2D col)
+    {
+
+        float y = BallHitPaddleWhere(transform.position,
+            col.transform.position,
+            col.collider.bounds.size.y);
+
+        Vector2 dir = new Vector2();
+
+        if(col.gameObject.name == "LeftPaddle")
+        {
+            dir = new Vector2(1, y).normalized;
+        }
+        if (col.gameObject.name == "RightPaddle")
+        {
+            dir = new Vector2(-1, y).normalized;
+        }
+
+        rigidBody.velocity = dir * speed;
+
+        SoundManager.Instace.PlayOneShot(SoundManager.Instace.hitPaddleBloop);
+    }
+
+    float BallHitPaddleWhere(Vector2 ball, Vector2 paddle, float paddleHeight)
+    {
+        return (ball.y = paddle.y / paddleHeight);
+    }
 }
---
+++ Assets/scripts/SoundManager.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoundManager : MonoBehaviour {
+
+    public static SoundManager Instace = null;
+
+    public AudioClip goalBloop;
+    public AudioClip lossBuzz;
+    public AudioClip hitPaddleBloop;
+    public AudioClip winSound;
+    public AudioClip wallBloop;
+
+    private AudioSource soundEffectAudio;
+
+    // Use this for initialization
+    void Start() {
+
+        if (Instace == null)
+        {
+            Instace = this;
+        } else if (Instace != this)
+        {
+            Destroy(gameObject);
+        }
+
+        AudioSource[] sources = GetComponents<AudioSource>();
+
+        foreach (AudioSource source in sources)
+        {
+            soundEffectAudio = source;
+        }
+    }
+    
+    public void PlayOneShot(AudioClip clip)
+    {
+        soundEffectAudio.PlayOneShot(clip);
+    }
+}
